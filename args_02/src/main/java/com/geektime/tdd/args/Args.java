207846e0a57d6c3e8f403a326ca00c3fc0f4cc05
JiangTaoHou <925147954@qq.com>
2023-09-20 22:41:55 +0800

    1.编写正确的代码，使得测试通过

--- args_02/src/main/java/com/geektime/tdd/args/Args.java
+++ args_02/src/main/java/com/geektime/tdd/args/Args.java
@@ -2,14 +2,19 @@ package com.geektime.tdd.args;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
 
 public class Args {
-
+    @SuppressWarnings("unchecked")
     public static <T> T parse(Class<T> optionsClass, String... args) {
-        Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
-
         try {
-            return (T) constructor.newInstance(true);
+            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+            Parameter parameter = constructor.getParameters()[0];
+            Option option = parameter.getAnnotation(Option.class);//这个就是l,p,d,传的参数是-l,-p,-d,
+            List<String> arguments = Arrays.asList(args);
+            return (T) constructor.newInstance(arguments.contains("-" +option.value()));//在这里进行拼接
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
