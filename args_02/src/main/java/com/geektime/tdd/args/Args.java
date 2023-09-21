fcbe9ac01c5b02f68ccc88b09d968079ad177f26
JiangTaoHou <925147954@qq.com>
2023-09-21 20:47:43 +0800

    1.现在先抽取出方法，准备使用多态

--- args_02/src/main/java/com/geektime/tdd/args/Args.java
+++ args_02/src/main/java/com/geektime/tdd/args/Args.java
@@ -19,7 +19,8 @@ public class Args {
             //最后根据顺序，去通过反射去创建对象
             /**为什么更改顺序也可以成功解析？因为constructor.getParameters()获取的顺序是定下来的，所以是按照参数的顺序
              * 来进行访问的，所以你哪个在先哪个在后，是没有关系的，和你的自己定义的那个record是有关系的*/
-            Object[] values = Arrays.stream(constructor.getParameters()).map(it -> parseOption(arguments, it)).toArray();
+            Object[] values = Arrays.stream(constructor.getParameters())
+                    .map(it -> parseOption(arguments, it)).toArray();
 
             return (T) constructor.newInstance(values);
         } catch (Exception e) {
@@ -31,18 +32,37 @@ public class Args {
         Object value = null;
         Option option = parameter.getAnnotation(Option.class);//这个就是l,p,d,传的参数是-l,-p,-d,
         if(parameter.getType() == boolean.class){
-            value = arguments.contains("-" + option.value());
+            value = parseBoolean(arguments, option);
         }
         if(parameter.getType() == int.class){
             //获取-p的索引
-            int index = arguments.indexOf("-" + option.value());
-            //那-p后面跟着的8080就是index的位置 + 1了，然后这个获取到是String类型的，需要转换为Int类型的
-            value = Integer.valueOf(arguments.get(index + 1));
+            value = parseInt(arguments, option);
         }
         if(parameter.getType() == String.class){
-            int index = arguments.indexOf("-" + option.value());
-            value = arguments.get(index + 1);
+            value = parseString(arguments, option);
         }
+        return value;
+    }
+
+
+    private static Object parseString(List<String> arguments, Option option) {
+        Object value;
+        int index = arguments.indexOf("-" + option.value());
+        value = arguments.get(index + 1);
+        return value;
+    }
+
+    private static Object parseBoolean(List<String> arguments, Option option) {
+        Object value;
+        value = arguments.contains("-" + option.value());
+        return value;
+    }
+
+    private static Object parseInt(List<String> arguments, Option option) {
+        Object value;
+        int index = arguments.indexOf("-" + option.value());
+        //那-p后面跟着的8080就是index的位置 + 1了，然后这个获取到是String类型的，需要转换为Int类型的
+        value = Integer.valueOf(arguments.get(index + 1));
         return value;
     }
 }
