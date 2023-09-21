f3c0d21bece03ccce75030781f2c2e7151511d3c
JiangTaoHou <925147954@qq.com>
2023-09-21 22:06:31 +0800

    1.创建构造函数，有参数和没参数的

--- args_02/src/main/java/com/geektime/tdd/args/IntOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/IntOptionParser.java
@@ -6,6 +6,14 @@ import java.util.function.Function;
 class IntOptionParser implements OptionParser {
     Function<String, Object> valueParser = Integer::parseInt;
 
+    public IntOptionParser() {
+
+    }
+
+    public IntOptionParser(Function<String, Object> valueParser) {
+        this.valueParser = valueParser;
+    }
+
     @Override
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
