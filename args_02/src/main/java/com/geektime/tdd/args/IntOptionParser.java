d55b7dc960743a506cc5c84376c8947133be4824
JiangTaoHou <925147954@qq.com>
2023-09-21 22:04:10 +0800

    1.introduce function variable,引入方法变量

--- args_02/src/main/java/com/geektime/tdd/args/IntOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/IntOptionParser.java
@@ -1,6 +1,7 @@
 package com.geektime.tdd.args;
 
 import java.util.List;
+import java.util.function.Function;
 
 class IntOptionParser implements OptionParser {
     @Override
@@ -11,6 +12,7 @@ class IntOptionParser implements OptionParser {
     }
 
     protected Object parseValue(String value) {
-        return Integer.valueOf(value);
+        Function<String, Integer> valueParser = value1 -> Integer.valueOf(value1);
+        return valueParser.apply(value);
     }
 }
