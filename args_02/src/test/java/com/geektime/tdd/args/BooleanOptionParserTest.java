9744c0b35ffea6b630d367d296580f7b9d4f72c3
JiangTaoHou <925147954@qq.com>
2023-09-21 23:26:39 +0800

    1.新加的这个东西，其实是取不到第三个值的，和传两个是一样的，所以这个测试是没必要存在的

--- args_02/src/test/java/com/geektime/tdd/args/BooleanOptionParserTest.java
+++ args_02/src/test/java/com/geektime/tdd/args/BooleanOptionParserTest.java
@@ -18,6 +18,13 @@ public class BooleanOptionParserTest {
             () -> new BooleanOptionParser().parse(asList("-l", "t"), option("l")));
         assertEquals("l", e.getOption());
     }
+
+    @Test
+    public void should_not_accept_extra_arguments_for_boolean_option() {
+        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class,
+                () -> new BooleanOptionParser().parse(asList("-l", "t","f"), option("l")));
+        assertEquals("l", e.getOption());
+    }
     static Option option(String value){
         return new Option(){
 
