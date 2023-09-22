1168068b3fb5c56cdb5b8b3449b21f7d4e6ea197
JiangTaoHou <925147954@qq.com>
2023-09-22 22:49:49 +0800

    1. -p 8080 8081 这个index就是-p的index,所以如果是索引加2，就是获取多的那个参数，没有超过传的参数的情况， 那么就获取到了，如果获取到了，并且不是以为-p开头，那么就是参数多了。

--- args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
@@ -14,6 +14,8 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
+        // -p 8080 8081 这个index就是-p的index,所以如果是索引加2，就是获取多的那个参数，没有超过传的参数的情况，
+        //那么就获取到了，如果获取到了，并且不是以为-p开头，那么就是参数多了
         if((index + 2) < arguments.size() && !arguments.get(index + 2).startsWith("-")) throw new TooManyArgumentsException(option.value());
         return valueParser.apply(arguments.get(index + 1));
     }
