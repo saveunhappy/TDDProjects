f6c779f0b3cb0430ab70912f725b5ae224bdbea1
JiangTaoHou <925147954@qq.com>
2023-09-22 23:24:51 +0800

    1.增加属性，返回，增加构造函数

--- args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
@@ -6,16 +6,23 @@ import java.util.function.Function;
 class SingleValueOptionParser<T> implements OptionParser<T> {
     Function<String, T> valueParser;
 
+    T defaultValue;
+
 
     public SingleValueOptionParser(Function<String, T> valueParser) {
         this.valueParser = valueParser;
+    }
+
+    public SingleValueOptionParser(Function<String, T> valueParser, T defaultValue) {
+        this.valueParser = valueParser;
+        this.defaultValue = defaultValue;
     }
 
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
 
-        if(index == -1) return ?;
+        if(index == -1) return defaultValue;
         //如果索引+1，按这里说就是1，size也是1，那么就说明只有一个参数，那么也报错
         if(index + 1 == arguments.size() || arguments.get(index + 1).startsWith("-")) throw new InsufficientException(option.value());
         // -p 8080 8081 这个index就是-p的index,所以如果是索引加2，就是获取多的那个参数，没有超过传的参数的情况，size是个数
