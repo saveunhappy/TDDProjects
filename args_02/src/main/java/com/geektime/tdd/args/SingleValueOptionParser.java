92c1c9ed2c19d7a6e0f49919d56df989a98f6788
JiangTaoHou <925147954@qq.com>
2023-09-25 22:05:20 +0800

    1.再变成泛型的，static的

--- args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
@@ -26,10 +26,10 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
 
     @Override
     public T parse(List<String> arguments, Option option) {
-        return getT(arguments, option);
+        return getT(arguments, option, valueParser, defaultValue);
     }
 
-    private T getT(List<String> arguments, Option option) {
+    private static <T> T getT(List<String> arguments, Option option, Function<String, T> valueParser, T defaultValue) {
         return values(arguments, option, 1).map(it -> parseValue(it.get(0), valueParser)).orElse(defaultValue);
     }
 
