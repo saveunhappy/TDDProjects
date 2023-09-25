bf51f0ca9fee16226a14b68e97e986dfeff26851
JiangTaoHou <925147954@qq.com>
2023-09-25 23:07:59 +0800

    1.intFunction是把一个列表转换为一个数组大小为int个数的数组，orElse(generator.apply(0))代表没有参数，那么数组的长度就是0

--- args_02/src/main/java/com/geektime/tdd/args/OptionParsers.java
+++ args_02/src/main/java/com/geektime/tdd/args/OptionParsers.java
@@ -3,6 +3,7 @@ package com.geektime.tdd.args;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.stream.IntStream;
 
 class OptionParsers {
@@ -12,7 +13,20 @@ class OptionParsers {
     }
 
     public static <T> OptionParser<T> unary(T defaultValue, Function<String, T> valueParser) {
-        return (arguments, option) -> values(arguments, option, 1).map(it -> parseValue(it.get(0), valueParser)).orElse(defaultValue);
+        return (arguments, option) -> values(arguments, option, 1)
+                .map(it -> parseValue(option,it.get(0), valueParser)).orElse(defaultValue);
+    }
+
+    public static <T> OptionParser<T[]> list(IntFunction<T[]> generator, Function<String, T> valueParser) {
+        return (arguments, option) -> values(arguments, option)
+                .map(it -> it.stream().map(value -> parseValue(option,value,valueParser)).toArray(generator))
+                .orElse(generator.apply(0));
+    }
+
+    private static Optional<List<String>> values(List<String> arguments, Option option) {
+        int index = arguments.indexOf("-" + option.value());
+        //list不需要限制参数的个数，多个是允许的
+        return Optional.ofNullable(index == -1 ? null : values(arguments, index));
     }
 
     private static Optional<List<String>> values(List<String> arguments, Option option, int expectedSize) {
@@ -26,10 +40,14 @@ class OptionParsers {
         return Optional.of(values);
     }
 
-    private static <T> T parseValue(String value, Function<String, T> valueParser) {
-        return valueParser.apply(value);
-    }
 
+    private static <T> T parseValue(Option option, String value, Function<String, T> valueParser) {
+        try {
+            return valueParser.apply(value);
+        } catch (Exception e) {
+            throw new IllegalValueException(option.value(), value);
+        }
+    }
     private static List<String> values(List<String> arguments, int index) {
         int followingFlag = IntStream.range(index + 1, arguments.size())
                 .filter(it -> arguments.get(it).startsWith("-"))
--- args_02/src/test/java/com/geektime/tdd/args/OptionParsersTest.java
+++ args_02/src/test/java/com/geektime/tdd/args/OptionParsersTest.java
@@ -14,44 +14,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class OptionParsersTest {
     @Nested
-    class UnaryOptionParser{
+    class UnaryOptionParser {
 
-    @Test//Sad path
-    public void should_not_accept_extra_argument_for_single_valued_option() throws Exception {
-        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () ->
-                OptionParsers.unary(0, Integer::parseInt)
-                        .parse(asList("-p", "8080", "8081"), option("p")));
-        assertEquals("p", e.getOption());
+        @Test//Sad path
+        public void should_not_accept_extra_argument_for_single_valued_option() throws Exception {
+            TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () ->
+                    OptionParsers.unary(0, Integer::parseInt)
+                            .parse(asList("-p", "8080", "8081"), option("p")));
+            assertEquals("p", e.getOption());
+        }
+
+        @ParameterizedTest//Sad path
+        @ValueSource(strings = {"-p -l", "-p"})
+        public void should_not_accept_insufficient_argument_for_single_valued_option(String arguments) throws Exception {
+            InsufficientException e = assertThrows(InsufficientException.class, () ->
+                    OptionParsers.unary(0, Integer::parseInt)
+                            .parse(asList(arguments.split(" ")), option("p")));
+            assertEquals("p", e.getOption());
+        }
+
+        @Test//Default value
+        public void should_set_default_value_to_0_for_int_option() throws Exception {
+            Function<String, Object> whatever = (it) -> null;
+            Object defaultValue = new Object();
+            assertSame(defaultValue, OptionParsers.unary(defaultValue, whatever).parse(asList(), option("-p")));
+        }
+
+        @Test//Happy path
+        public void should_parse_value_if_flag_present() throws Exception {
+            Object parsed = new Object();
+            //String转换为Object
+            Function<String, Object> parse = (it) -> parsed;
+            Object whatever = new Object();
+            //这个是测试正常情况，我们要测的是Function，所以默认值我们是不关心的，默认是就是whatever
+            //经过function的函数，我们默认返回parsed,所以用assertSame，证明是返回了我们想要的
+            assertSame(parsed, OptionParsers.unary(whatever, parse)
+                    .parse(asList("-p", "8080"), option("p")));
+        }
     }
 
-    @ParameterizedTest//Sad path
-    @ValueSource(strings = {"-p -l", "-p"})
-    public void should_not_accept_insufficient_argument_for_single_valued_option(String arguments) throws Exception {
-        InsufficientException e = assertThrows(InsufficientException.class, () ->
-                OptionParsers.unary(0, Integer::parseInt)
-                        .parse(asList(arguments.split(" ")), option("p")));
-        assertEquals("p", e.getOption());
-    }
-
-    @Test//Default value
-    public void should_set_default_value_to_0_for_int_option() throws Exception {
-        Function<String,Object> whatever = (it) -> null;
-        Object defaultValue = new Object();
-        assertSame(defaultValue, OptionParsers.unary(defaultValue, whatever).parse(asList(), option("-p")));
-    }
-
-    @Test//Happy path
-    public void should_parse_value_if_flag_present() throws Exception {
-        Object parsed = new Object();
-        //String转换为Object
-        Function<String,Object> parse = (it) -> parsed;
-        Object whatever = new Object();
-        //这个是测试正常情况，我们要测的是Function，所以默认值我们是不关心的，默认是就是whatever
-        //经过function的函数，我们默认返回parsed,所以用assertSame，证明是返回了我们想要的
-        assertSame(parsed, OptionParsers.unary(whatever, parse)
-                .parse(asList("-p","8080"), option("p")));
-    }
-    }
     @Nested
     class BooleanOptionParserTest {
 
@@ -59,7 +60,7 @@ public class OptionParsersTest {
         @Test//Sad path
         public void should_not_accept_extra_argument_for_boolean_option() {
             TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class,
-                () -> OptionParsers.bool().parse(asList("-l", "t"), option("l")));
+                    () -> OptionParsers.bool().parse(asList("-l", "t"), option("l")));
             assertEquals("l", e.getOption());
         }
 
@@ -73,8 +74,9 @@ public class OptionParsersTest {
             assertTrue(OptionParsers.bool().parse(asList("-l"), option("l")));
 
         }
-        static Option option(String value){
-            return new Option(){
+
+        static Option option(String value) {
+            return new Option() {
 
                 @Override
                 public Class<? extends Annotation> annotationType() {
@@ -89,5 +91,33 @@ public class OptionParsersTest {
         }
 
 
+    }
+
+    @Nested
+    class ListOptionParser {
+        //TODO -g "this" "is" {"this","is"}
+        @Test
+        public void should_parse_list_value() throws Exception {
+            String[] value = OptionParsers.list(String[]::new, String::valueOf)
+                    .parse(asList("-g", "this", "is"), option("g"));
+            assertArrayEquals(new String[]{"this","is"},value);
+        }
+        //TODO -default value []
+        //TODO -d a throw exception  a不是数字，应该是数字的
+    }
+
+    static Option option(String value) {
+        return new Option() {
+
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return Option.class;
+            }
+
+            @Override
+            public String value() {
+                return value;
+            }
+        };
     }
 }
