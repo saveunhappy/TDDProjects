7b1740bcd3344d5fad68d77c7e2a9083e8a8a382
JiangTaoHou <925147954@qq.com>
2023-09-26 23:41:47 +0800

    1.移动一下配置类，看的更清晰

--- args_02/src/main/java/com/geektime/tdd/args/Args.java
+++ args_02/src/main/java/com/geektime/tdd/args/Args.java
@@ -8,6 +8,13 @@ import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public class Args<T> {
+    private static Map<Class<?>, OptionParser> PARSER = Map.of(
+            boolean.class, OptionParsers.bool(),
+            int.class, OptionParsers.unary(0, Integer::parseInt),
+            String.class, OptionParsers.unary("", String::valueOf),
+            String[].class,OptionParsers.list(String[]::new,String::valueOf),
+            Integer[].class,OptionParsers.list(Integer[]::new,Integer::parseInt)
+    );
 
     public static <T> T parse(Class<T> optionsClass, String... args) {
 
@@ -54,13 +61,7 @@ public class Args<T> {
         return parser.get(type).parse(arguments, option);
     }
 
-    private static Map<Class<?>, OptionParser> PARSER = java.util.Map.of(
-            boolean.class, OptionParsers.bool(),
-            int.class, OptionParsers.unary(0, Integer::parseInt),
-            String.class, OptionParsers.unary("", String::valueOf),
-            String[].class,OptionParsers.list(String[]::new,String::valueOf),
-            Integer[].class,OptionParsers.list(Integer[]::new,Integer::parseInt)
-            );
+
 
 
 }
