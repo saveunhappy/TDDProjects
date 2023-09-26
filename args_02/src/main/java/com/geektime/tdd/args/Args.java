aa20d7d3476f8192d07bd4c050dd02fe0b736591
JiangTaoHou <925147954@qq.com>
2023-09-26 23:24:30 +0800

    1.然后在调用处，先生成变量

--- args_02/src/main/java/com/geektime/tdd/args/Args.java
+++ args_02/src/main/java/com/geektime/tdd/args/Args.java
@@ -21,7 +21,10 @@ public class Args {
             /**为什么更改顺序也可以成功解析？因为constructor.getParameters()获取的顺序是定下来的，所以是按照参数的顺序
              * 来进行访问的，所以你哪个在先哪个在后，是没有关系的，和你的自己定义的那个record是有关系的*/
             Object[] values = Arrays.stream(constructor.getParameters())
-                    .map(it -> parseOption(arguments, it, PARSER)).toArray();
+                    .map(it -> {
+                        Map<Class<?>, OptionParser> parser = PARSER;
+                        return parseOption(arguments, it, parser);
+                    }).toArray();
 
             return (T) constructor.newInstance(values);
         }catch (IllegalOptionException e){
