839aee71523fe11f87486f7f2a1762007af4a522
JiangTaoHou <925147954@qq.com>
2023-10-16 16:55:16 +0800

    1.增加注释

--- args_02/src/main/java/com/geektime/tdd/args/OptionParsers.java
+++ args_02/src/main/java/com/geektime/tdd/args/OptionParsers.java
@@ -19,13 +19,15 @@ class OptionParsers {
 
     public static <T> OptionParser<T[]> list(IntFunction<T[]> generator, Function<String, T> valueParser) {
         return (arguments, option) -> values(arguments, option)
+                //之前的unary是要求只有一个参数，-p 8080,-d /usr/log，这个是list，[this,is]，values就是list，所以可以进行stream，
+                // generator就是创建什么类型的数组，外面传过来，使用Functional的
                 .map(it -> it.stream().map(value -> parseValue(option, value, valueParser)).toArray(generator))
                 .orElse(generator.apply(0));
     }
 
     private static Optional<List<String>> values(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
-        //list不需要限制参数的个数，多个是允许的
+        //list不需要限制参数的个数，多个是允许的，
         return Optional.ofNullable(index == -1 ? null : values(arguments, index));
     }
 
@@ -52,13 +54,6 @@ class OptionParsers {
         int followingFlag = IntStream.range(index + 1, arguments.size())
                 .filter(it -> arguments.get(it).matches("^-[a-zA-Z-]+$"))
                 .findFirst().orElse(arguments.size());
-        //如果找到了刚开始的那个flag，然后找到接下来的那个flag，如果存在，比如size等于1，那么就是一个参数，是正常的
-        //如果第一个标志后面紧跟着一个标志，那么就是返回的参数的个数，第一个flag的位置是0，index + 1就是1，如果中间没有参数，
-        // 比如-p -l，并且起始位置是从Index + 1开始的，index是flag的位置，加1就是下一个的位置，
-        // 那么.filter(it -> arguments.get(it).startsWith("-"))获取到的下一个的位置就是 -l的位置，所以following的位置就是1
-        //这个就是从1到1的区间，那就是0，小于1，就是参数不够，如果是-p,那么找不到arguments.get(it).startsWith("-")，
-        // 还是返回个数，1个，那还是一样的，所以，这个就是参数不够
-        //如果是 -p 8080 8081 -d /usr/log,范围是从1-3,那么就是两个参数，那么就是多参数了
         return arguments.subList(index + 1, followingFlag);
     }
 
