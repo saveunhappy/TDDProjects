526467ca0117f657f9a3616ad6dd8716e7125085
JiangTaoHou <925147954@qq.com>
2023-10-18 22:29:09 +0800

    1.移动过去

--- args_02/src/main/java/com/geektime/tdd/args/Args.java
+++ args_02/src/main/java/com/geektime/tdd/args/Args.java
@@ -1,12 +1,16 @@
 package com.geektime.tdd.args;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public class Args {
 
     public static <T> T parse(Class<T> optionsClass, String... args) {
-        return OptionClass.getT(PARSER, optionsClass, args);
+        return getT(PARSER, optionsClass, args);
     }
 
     private static Map<Class<?>, OptionParser> PARSER = Map.of(
@@ -17,4 +21,29 @@ public class Args {
             Integer[].class, OptionParsers.list(Integer::parseInt, Integer[]::new)
     );
 
+    public static <T> T getT(Map<Class<?>, OptionParser> parsers, Class<T> optionsClass, String[] args) {
+        try {
+            List<String> arguments = Arrays.asList(args);
+            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+            Object[] values = Arrays.stream(constructor.getParameters())
+                    .map(it -> parseOption(arguments, it, parsers)).toArray();
+
+            return (T) constructor.newInstance(values);
+        } catch (IllegalOptionException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Object parseOption(List<String> arguments, Parameter parameter, Map<Class<?>, OptionParser> parsers) {
+        if (!parameter.isAnnotationPresent(Option.class)) throw new IllegalOptionException(parameter.getName());
+        Option option = parameter.getAnnotation(Option.class);
+        //这个就是l,p,d,传的参数是-l,-p,-d,
+        Class<?> type = parameter.getType();
+        if (!parsers.containsKey(parameter.getType())) {
+            throw new UnsupportedOptionTypeException(option.value(), parameter.getType());
+        }
+        return parsers.get(type).parse(arguments, parameter.getAnnotation(Option.class));
+    }
 }
--- args_02/src/main/java/com/geektime/tdd/args/OptionClass.java
+++ args_02/src/main/java/com/geektime/tdd/args/OptionClass.java
@@ -1,38 +1,6 @@
 package com.geektime.tdd.args;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Parameter;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
 class OptionClass<T> {
-
-    public static <T> T getT(Map<Class<?>, OptionParser> parsers, Class<T> optionsClass, String[] args) {
-        try {
-            List<String> arguments = Arrays.asList(args);
-            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
-            Object[] values = Arrays.stream(constructor.getParameters())
-                    .map(it -> parseOption(arguments, it, parsers)).toArray();
-
-            return (T) constructor.newInstance(values);
-        } catch (IllegalOptionException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Object parseOption(List<String> arguments, Parameter parameter, Map<Class<?>, OptionParser> parsers) {
-        if (!parameter.isAnnotationPresent(Option.class)) throw new IllegalOptionException(parameter.getName());
-        Option option = parameter.getAnnotation(Option.class);
-        //这个就是l,p,d,传的参数是-l,-p,-d,
-        Class<?> type = parameter.getType();
-        if (!parsers.containsKey(parameter.getType())) {
-            throw new UnsupportedOptionTypeException(option.value(), parameter.getType());
-        }
-        return parsers.get(type).parse(arguments, parameter.getAnnotation(Option.class));
-    }
 
 
 }
