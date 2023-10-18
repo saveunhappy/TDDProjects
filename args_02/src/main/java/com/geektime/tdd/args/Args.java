54f2afab0fa74ff7afb3a9e2305ab2b8f083d543
JiangTaoHou <925147954@qq.com>
2023-10-18 21:24:53 +0800

    1.抽取出来了，尽情的再那个里面改吧，不会波及到其他的地方

--- args_02/src/main/java/com/geektime/tdd/args/Args.java
+++ args_02/src/main/java/com/geektime/tdd/args/Args.java
@@ -16,14 +16,7 @@ public class Args {
     private static <T> T getT(Class<T> optionsClass, String[] args) {
         try {
             List<String> arguments = Arrays.asList(args);
-            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
-            //为什么这样就可以了？抽取出了一个方法，那么arguments就是获取所有的参数了
-            //然后通过stream的方式，每个参数都去调用parseOption的方式得到从arguments中筛选出一个对应的值，
-            //如果是-l就返回boolean值，
-            //如果是-p,那么也是通过index的方式去获取他后面的参数的，同理，-d也是获取他后面的
-            //最后根据顺序，去通过反射去创建对象
-            /**为什么更改顺序也可以成功解析？因为constructor.getParameters()获取的顺序是定下来的，所以是按照参数的顺序
-             * 来进行访问的，所以你哪个在先哪个在后，是没有关系的，和你的自己定义的那个record是有关系的*/
+            Constructor<?> constructor = new OptionClass<>(optionsClass).optionsClass.getDeclaredConstructors()[0];
             Object[] values = Arrays.stream(constructor.getParameters())
                     .map(it -> parseOption(arguments, it)).toArray();
 
@@ -36,6 +29,13 @@ public class Args {
         }
     }
 
+    static class OptionClass<T>{
+        Class<T> optionsClass;
+
+        public OptionClass(Class<T> optionsClass) {
+            this.optionsClass = optionsClass;
+        }
+    }
     private static Object parseOption(List<String> arguments, Parameter parameter) {
         Map<Class<?>, OptionParser> parsers = PARSER;
         if(!parameter.isAnnotationPresent(Option.class)) throw new IllegalOptionException(parameter.getName());
