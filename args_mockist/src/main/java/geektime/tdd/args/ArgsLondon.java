447698bed81a7aee12c916b244d90913dd17f9ea
JiangTaoHou <925147954@qq.com>
2023-10-24 00:04:06 +0800

    1.增加注释，其实就是按照他的流程下来，一步一步调用哪个方法，返回什么东西，最后再断言，这个是有用的，不是单纯的mock数据，这个是验证你调用了这个方法，返回了什么值

--- args_mockist/src/main/java/geektime/tdd/args/ArgsLondon.java
+++ args_mockist/src/main/java/geektime/tdd/args/ArgsLondon.java
@@ -4,23 +4,22 @@ import java.util.Arrays;
 
 public class ArgsLondon<T> {
 
-//    private ValueRetrieverLondon retriever;
-//    private OptionParserLondon parser;
-//    private OptionClassLondon<T> optionClass;
+    private ValueRetrieverLondon retriever;
+    private OptionParserLondon parser;
+    private OptionClassLondon<T> optionClass;
 
     public ArgsLondon(ValueRetrieverLondon retriever, OptionParserLondon parser, OptionClassLondon<T> optionClass) {
-//        this.retriever = retriever;
-//        this.parser = parser;
-//        this.optionClass = optionClass;
+        this.retriever = retriever;
+        this.parser = parser;
+        this.optionClass = optionClass;
     }
 
     public T parse(String... args) {
-//
-//        return optionClass.create(Arrays.stream(optionClass.getOptionNames())
-//                .map(name -> parser.parse(
-//                        optionClass.getOptionType(name),
-//                        retriever.getValue(name, args))).toArray(Object[]::new));
-        return null;
+
+        return optionClass.create(Arrays.stream(optionClass.getOptionNames())
+                .map(name -> parser.parse(
+                        optionClass.getOptionType(name),
+                        retriever.getValue(name, args))).toArray(Object[]::new));
     }
 
 }
--- args_mockist/src/test/java/geektime/tdd/args/ArgsLondonTest.java
+++ args_mockist/src/test/java/geektime/tdd/args/ArgsLondonTest.java
@@ -13,12 +13,15 @@ public class ArgsLondonTest {
         ValueRetrieverLondon retriever = mock(ValueRetrieverLondon.class);
         OptionClassLondon<IntOption> optionClass = mock(OptionClassLondon.class);
         OptionParserLondon parser = mock(OptionParserLondon.class);
-
+        //这个就是获取参数列表
         when(optionClass.getOptionNames()).thenReturn(new String[]{"p"});
         when(optionClass.getOptionType(eq("p"))).thenReturn(int.class);
+        //这个就是注解上的@Option("p")后面那个就是-option.value，然后返回String的。
         when(retriever.getValue(eq("p"), eq(new String[]{"-p", "8080"})))
                 .thenReturn(new String[]{"8080"});
+        //这个就是Integer.parseInt
         when(parser.parse(eq(int.class), eq(new String[]{"8080"}))).thenReturn(8080);
+        //这个就是toArray，需要的就是一个Object，返回一个IntOption
         when(optionClass.create(eq(new Object[]{8080}))).thenReturn(new IntOption(8080));
 
         ArgsLondon<IntOption> args = new ArgsLondon<>(retriever, parser, optionClass);
