0b4f51bb4676b8033a18b5684f1062d75b1b1e30
JiangTaoHou <925147954@qq.com>
2023-10-26 22:57:31 +0800

    1.步子不要迈得太大了，一个一个来，先替换一个

--- diContainer/src/main/java/com/geektime/tdd/Context.java
+++ diContainer/src/main/java/com/geektime/tdd/Context.java
@@ -8,28 +8,29 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Context {
-    private Map<Class<?>, Object> components = new HashMap<>();
     private Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
     private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
     //这个是泛型方法，如果再类上加上了<T> 方法public T sayHay(){}这个是普通方法，下卖弄这个才是泛型方法
     //测试通过，就是把一个类的Class绑定到它的实例上面去。
     public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        components.put(type,instance);
+        providers.put(type, (Provider<ComponentType>) () -> instance);
     }
 
     //这个和    public static <T> T parse(Class<T> optionsClass, String... args) 一样的，只是泛型的名字变了。
+
+
+    public <ComponentType,ComponentImplementation extends ComponentType>
+    void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
+        componentImplementations.put(type,implementation);
+    }
+
     public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        if(components.containsKey(type)) return (ComponentType) components.get(type);
+        if(providers.containsKey(type)) return (ComponentType) providers.get(type).get();
         try {
             return (ComponentType) componentImplementations.get(type).getConstructor().newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public <ComponentType,ComponentImplementation extends ComponentType>
-    void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
-        componentImplementations.put(type,implementation);
     }
 }
