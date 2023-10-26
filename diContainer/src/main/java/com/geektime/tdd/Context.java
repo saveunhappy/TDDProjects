34db8ef3e15ff93994f2c67adc817e2aa330c887
JiangTaoHou <925147954@qq.com>
2023-10-26 22:50:03 +0800

    1.测试通过，不惜犯下罪恶，我们多创建了一个Map，因为类型不一样

--- diContainer/src/main/java/com/geektime/tdd/Context.java
+++ diContainer/src/main/java/com/geektime/tdd/Context.java
@@ -1,11 +1,13 @@
 package com.geektime.tdd;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 public class Context {
     private Map<Class<?>, Object> components = new HashMap<>();
+    private Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
 
     //这个是泛型方法，如果再类上加上了<T> 方法public T sayHay(){}这个是普通方法，下卖弄这个才是泛型方法
     //测试通过，就是把一个类的Class绑定到它的实例上面去。
@@ -15,6 +17,16 @@ public class Context {
 
     //这个和    public static <T> T parse(Class<T> optionsClass, String... args) 一样的，只是泛型的名字变了。
     public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        return (ComponentType) components.get(type);
+        if(components.containsKey(type)) return (ComponentType) components.get(type);
+        try {
+            return (ComponentType) componentImplementations.get(type).getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <ComponentType,ComponentImplementation extends ComponentType>
+    void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
+        componentImplementations.put(type,implementation);
     }
 }
--- diContainer/src/test/java/com/geektime/tdd/ContainerTest.java
+++ diContainer/src/test/java/com/geektime/tdd/ContainerTest.java
@@ -3,13 +3,17 @@ package com.geektime.tdd;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 
 class ContainerTest {
     interface Component{
 
     }
 
+    static class ComponentWithDefaultConstructor implements Component{
+        public ComponentWithDefaultConstructor() {
+        }
+    }
 
     @Nested
     public class ComponentConstruction{
@@ -30,7 +34,16 @@ class ContainerTest {
         //TODO: interface
         @Nested
         public class ConstructorInjection{
-            //TODO: no args constructor
+            //TODO: no args constructor 无依赖的组件应该通过默认构造函数生成组件实例
+            @Test
+            public void should_bind_type_to_a_class_with_default_constructor() throws Exception{
+                Context context = new Context();
+                context.bind(Component.class,ComponentWithDefaultConstructor.class);
+                Component instance = context.get(Component.class);
+                assertNotNull(instance);
+                assertTrue(instance instanceof ComponentWithDefaultConstructor);
+
+            }
             //TODO: with dependencies
             //TODO: A->B->C
 
