830403066e1a320be3ed890956f4bf073c81496c
JiangTaoHou <925147954@qq.com>
2023-10-26 21:59:42 +0800

    1.初始化项目

---
+++ _01.di.container/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    `java-library`
+    "jacoco"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+//    maven(url("https://maven.aliyun.com/repository/public/"));
+//    maven(url("https://maven.aliyun.com/repository/spring/"));
+
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("jakarta.inject:jakarta.inject-api:2.0.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
+    testImplementation("org.junit.platform:junit-platform-runner:1.8.2")
+    testImplementation("jakarta.inject:jakarta.inject-tck:2.0.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
