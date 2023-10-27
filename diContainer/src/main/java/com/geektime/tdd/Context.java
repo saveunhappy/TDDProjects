aaecc80d0f526ef15ca7ad274fb6782828207145
JiangTaoHou <925147954@qq.com>
2023-10-27 22:24:03 +0800

    1.修改泛型的名字

--- diContainer/src/main/java/com/geektime/tdd/Context.java
+++ diContainer/src/main/java/com/geektime/tdd/Context.java
@@ -10,26 +10,26 @@ public class Context {
 
     //这个是泛型方法，如果再类上加上了<T> 方法public T sayHay(){}这个是普通方法，下卖弄这个才是泛型方法
     //测试通过，就是把一个类的Class绑定到它的实例上面去。
-    public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        providers.put(type, (Provider<ComponentType>) () -> instance);
+    public <Type> void bind(Class<Type> type, Type instance) {
+        providers.put(type, (Provider<Type>) () -> instance);
     }
 
     //这个和    public static <T> T parse(Class<T> optionsClass, String... args) 一样的，只是泛型的名字变了。
 
 
-    public <ComponentType,ComponentImplementation extends ComponentType>
-    void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
-        providers.put(type, (Provider<ComponentType>) () -> {
+    public <Type,Implementation extends Type>
+    void bind(Class<Type> type, Class<Implementation> implementation) {
+        providers.put(type, (Provider<Type>) () -> {
             try {
-                return (ComponentType) ((Class<?>) implementation).getConstructor().newInstance();
+                return (Type) ((Class<?>) implementation).getConstructor().newInstance();
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
     }
 
-    public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        return (ComponentType) providers.get(type).get();
+    public <Type> Type get(Class<Type> type) {
+        return (Type) providers.get(type).get();
     }
 
 }
