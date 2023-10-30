d052769dd973a5655ea38f75b0035709abde0768
JiangTaoHou <925147954@qq.com>
2023-10-30 22:01:02 +0800

    1.这个时候就是手动修改了，去delegate刚自己写的那个方法

--- diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
+++ diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
@@ -38,7 +38,7 @@ public class ContextConfig implements Context {
 
     @Override
     public <Type> Optional<Type> get(Class<Type> type) {
-        return Optional.ofNullable(providers.get(type)).map(provider -> (Type) provider.get());
+        return getContext().get(type);
     }
 
 
