8373fc1036a55ab4855b88d7d769b7cb55849149
JiangTaoHou <925147954@qq.com>
2023-10-31 22:05:29 +0800

    1.删除原来依赖没有注入的情况的代码，因为在检查循环依赖的时候会去检查是否有依赖没有注入的情况，

--- diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
+++ diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
@@ -33,15 +33,6 @@ public class ContextConfig {
     public Context getContext() {
         //这个dependencies中就是记录了所有的，还有你的参数中有的依赖，也去给你put进去，
         for (Class<?> component : dependencies.keySet()) {
-            for (Class<?> dependency : dependencies.get(component)) {
-                //就是你bind一个接口就得有一个实现类，如果你Component依赖了Dependency
-                //但是你没有bind过，那就在dependencies没有找到，就抛出异常,key是bind过的，value就是你的构造器参数
-                //也是一个Class，如果没有找到，那是不应该的，因为key和value都应该在dependencies中的
-                //Component->Dependency->String现在如果String没有，
-                // 但是依赖有两个Component:Dependency,Dependency:String
-                //dependencies.get(String)这个找不到，没有Bind过，所以报错
-                if (!dependencies.containsKey(dependency)) throw new DependencyNotFoundException(component, dependency);
-            }
             checkDependencies(component,new Stack<>());
         }
         return new Context() {
@@ -54,6 +45,12 @@ public class ContextConfig {
 
     private void checkDependencies(Class<?> component, Stack<Class<?>> visiting) {
         for (Class<?> dependency : dependencies.get(component)) {
+            //就是你bind一个接口就得有一个实现类，如果你Component依赖了Dependency
+            //但是你没有bind过，那就在dependencies没有找到，就抛出异常,key是bind过的，value就是你的构造器参数
+            //也是一个Class，如果没有找到，那是不应该的，因为key和value都应该在dependencies中的
+            //Component->Dependency->String现在如果String没有，
+            // 但是依赖有两个Component:Dependency,Dependency:String
+            //dependencies.get(String)这个找不到，没有Bind过，所以报错
             if (!dependencies.containsKey(dependency)) throw new DependencyNotFoundException(component, dependency);
             if (visiting.contains(dependency)) throw new CyclicDependenciesFoundException(visiting);
             visiting.push(dependency);
