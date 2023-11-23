c5eb8214ed82de15fb6737d0c17ee58d3ebbb91c
JiangTaoHou <925147954@qq.com>
2023-11-23 20:55:43 +0800

    1.依赖找不到的异常，构造器，字段，方法

--- diContainer/src/test/java/com/geektime/tdd/ContextTest.java
+++ diContainer/src/test/java/com/geektime/tdd/ContextTest.java
@@ -1,10 +1,7 @@
 package com.geektime.tdd;
 
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Named;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,8 +9,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Nested
 public class ContextTest {
@@ -46,12 +43,13 @@ public class ContextTest {
 
             Optional<Component> component = config.getContext().get(Component.class);
             assertTrue(component.isPresent());
-            assertSame(dependency,component.get().dependency());
+            assertSame(dependency, component.get().dependency());
         }
-        public static Stream<Arguments> should_bind_type_to_an_injectable_component(){
-            return Stream.of(Arguments.of(Named.of("Constructor injection",ConstructorInjection.class)),
-                    Arguments.of(Named.of("Field injection",FieldInjection.class)),
-                    Arguments.of(Named.of("Method injection",MethodInjection.class)));
+
+        public static Stream<Arguments> should_bind_type_to_an_injectable_component() {
+            return Stream.of(Arguments.of(Named.of("Constructor injection", ConstructorInjection.class)),
+                    Arguments.of(Named.of("Field injection", FieldInjection.class)),
+                    Arguments.of(Named.of("Method injection", MethodInjection.class)));
         }
 
 
@@ -61,6 +59,7 @@ public class ContextTest {
 
         static class ConstructorInjection implements Component {
             Dependency dependency;
+
             @Inject
             public ConstructorInjection(Dependency dependency) {
                 this.dependency = dependency;
@@ -81,8 +80,10 @@ public class ContextTest {
                 return dependency;
             }
         }
+
         static class MethodInjection implements Component {
             Dependency dependency;
+
             @Inject
             void install(Dependency dependency) {
                 this.dependency = dependency;
@@ -98,6 +99,48 @@ public class ContextTest {
         public void should_retrieve_empty_for_unbind_type() {
             Optional<Component> component = config.getContext().get(Component.class);
             assertTrue(component.isEmpty());
+        }
+    }
+
+    @Nested
+    public class DependencyCheck {
+        @ParameterizedTest
+        @MethodSource
+        public void should_throw_exception_if_dependency_not_found(Class<? extends Component> component) {
+            config.bind(Component.class, component);
+            DependencyNotFoundException exception = assertThrows(DependencyNotFoundException.class,
+                    () -> config.getContext());
+            //这个就是哪个组件找不到哪个依赖，具体看checkDependencies这个方法
+            //Field和Method找不到可以去看getDependency()这个方法，它是把构造器的参数
+            //字段，还有方法的参数都添加进去了，进行concat
+            assertEquals(Dependency.class, exception.getDependency());
+            assertEquals(Component.class, exception.getComponent());
+
+        }
+
+        public static Stream<Arguments> should_throw_exception_if_dependency_not_found() {
+            return Stream.of(Arguments.of(Named.of("inject Constructor", MissingDependencyConstructor.class)),
+                    Arguments.of(Named.of("inject Field", MissingDependencyField.class)),
+                    Arguments.of(Named.of("inject Method", MissingDependencyMethod.class)));
+        }
+
+        static class MissingDependencyConstructor implements Component {
+            @Inject
+            public MissingDependencyConstructor(Dependency dependency) {
+
+            }
+        }
+
+        static class MissingDependencyField implements Component {
+            @Inject
+            Dependency dependency;
+        }
+
+        static class MissingDependencyMethod implements Component {
+            @Inject
+            void install(Dependency dependency) {
+
+            }
         }
     }
 
