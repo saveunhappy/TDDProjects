3ba45de12c6907c199b50b751d74d1c26e13ed24
JiangTaoHou <925147954@qq.com>
2023-12-05 20:22:50 +0800

    1.从InjectionProvider拷贝过来，编译是不通过的

--- diContainer/src/main/java/com/geektime/tdd/Context.java
+++ diContainer/src/main/java/com/geektime/tdd/Context.java
@@ -1,10 +1,16 @@
 package com.geektime.tdd;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Optional;
 
 public interface Context {
     <Type> Optional<Type> get(Class<Type> type);
 
     Optional<Object> get(ParameterizedType type);
+
+    default Optional getType(Type type){
+        if (type instanceof ParameterizedType) return context.get((ParameterizedType) type).get();
+        return context.get((Class<?>) type).get();
+    }
 }
