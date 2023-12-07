9c2d5dd3c1bf02268150f59a285f3aa0a0f8a4b9
JiangTaoHou <925147954@qq.com>
2023-12-07 23:52:33 +0800

    1.重命名

--- diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
+++ diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
@@ -45,12 +45,12 @@ public class ContextConfig {
         //这个是去找的所有bind过的依赖，然后把所有的key的依赖都放到一个栈中去，这里是找的所有的依赖，如果之前有添加过
         //那就说明有环了，就是有循环依赖
         Class<?> componentType = component;
-        for (Context.Ref ref : providers.get(componentType).getDependenciesRef()) {
-            if (!providers.containsKey(ref.getComponent())) throw new DependencyNotFoundException(componentType, ref.getComponent());
-            if (!ref.isContainer()) {
-                if (visiting.contains(ref.getComponent())) throw new CyclicDependenciesFoundException(visiting);
-                visiting.push(ref.getComponent());
-                checkDependencies(ref.getComponent(), visiting);
+        for (Context.Ref dependency : providers.get(componentType).getDependenciesRef()) {
+            if (!providers.containsKey(dependency.getComponent())) throw new DependencyNotFoundException(componentType, dependency.getComponent());
+            if (!dependency.isContainer()) {
+                if (visiting.contains(dependency.getComponent())) throw new CyclicDependenciesFoundException(visiting);
+                visiting.push(dependency.getComponent());
+                checkDependencies(dependency.getComponent(), visiting);
                 visiting.pop();
             }
 
