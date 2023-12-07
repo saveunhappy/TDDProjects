d8ea45fc76863909f04c067322527d8b72f1bc26
JiangTaoHou <925147954@qq.com>
2023-12-08 00:20:05 +0800

    1.inline成原来的那个

--- diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
+++ diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
@@ -58,7 +58,12 @@ class InjectionProvider<T> implements ComponentProvider<T> {
     }
     @Override
     public List<Context.Ref> getDependenciesRef(){
-        return getDependencies().stream().map(Context.Ref::of).toList();
+        return concat(concat(stream(injectConstructor.getParameters()).map(Parameter::getParameterizedType),
+                        injectFields.stream().map(Field::getGenericType)),
+                //因为Constructor直接就是可以获取数组，所以不用flatMap,然后InjectMethod是List，所以要使用Stream
+                //那为什么Map不行呢？因为后面的m.getParameterTypes()返回的还是数组，你要把它变成一维的，所以要使用flatMap
+                injectMethods.stream().flatMap(p -> stream(p.getParameters()).map(Parameter::getParameterizedType)))
+                .toList().stream().map(Context.Ref::of).toList();
     }
 
     private static <T> List<Field> getInjectFields(Class<T> component) {
