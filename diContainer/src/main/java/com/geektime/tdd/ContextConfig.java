b7f4a1abe06cb4dee148b46eedcbc704d9718bad
JiangTaoHou <925147954@qq.com>
2023-12-12 21:09:25 +0800

    1.删除之前的，按理说该改名字了，但是这个时候没有改

--- diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
+++ diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
@@ -8,7 +8,6 @@ import java.util.*;
 import static java.util.Arrays.stream;
 
 public class ContextConfig {
-    private final Map<Class<?>, ComponentProvider<?>> providers = new HashMap<>();
     private final Map<Component, ComponentProvider<?>> components = new HashMap<>();
 
     public <Type> void bind(Class<Type> type, Type instance) {
