bfe4a61da4a2d677d86eca34a450af42e93311f7
JiangTaoHou <925147954@qq.com>
2023-12-13 22:01:52 +0800

    1.在之前的测试中，delegate成新的方法实现，只为移除之前的

--- diContainer/src/main/java/com/geektime/tdd/DependencyNotFoundException.java
+++ diContainer/src/main/java/com/geektime/tdd/DependencyNotFoundException.java
@@ -1,14 +1,15 @@
 package com.geektime.tdd;
 
 public class DependencyNotFoundException extends RuntimeException{
+    private Component componentComponent;
+    private Component dependencyComponent;
 
     public DependencyNotFoundException(Component componentComponent, Component dependencyComponent) {
         this.componentComponent = componentComponent;
         this.dependencyComponent = dependencyComponent;
     }
 
-    private Component componentComponent;
-    private Component dependencyComponent;
+
 
     public Class<?> getDependency() {
         return dependencyComponent.type();
