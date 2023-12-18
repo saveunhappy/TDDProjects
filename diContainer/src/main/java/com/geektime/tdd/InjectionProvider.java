0e27e15b42403fb75adb78edf0aa8cd0b72b421b
JiangTaoHou <925147954@qq.com>
2023-12-18 20:03:02 +0800

    1.测试通过

--- diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
+++ diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
@@ -50,11 +50,18 @@ class InjectionProvider<T> implements ComponentProvider<T> {
     @Override
     public List<ComponentRef> getDependencies(){
         return concat(concat(stream(injectConstructor.getParameters()).map(InjectionProvider::toComponentRef),
-                        injectFields.stream().map(Field::getGenericType).map(ComponentRef::of)),
+                        injectFields.stream().map(InjectionProvider::toComponentRef)),
                 //因为Constructor直接就是可以获取数组，所以不用flatMap,然后InjectMethod是List，所以要使用Stream
                 //那为什么Map不行呢？因为后面的m.getParameterTypes()返回的还是数组，你要把它变成一维的，所以要使用flatMap
                 injectMethods.stream().flatMap(p -> stream(p.getParameters()).map(InjectionProvider::toComponentRef)))
                 .toList();
+    }
+
+    private static ComponentRef toComponentRef(Field field) {
+        Annotation qualifier = stream(field.getAnnotations()).filter(a -> a.annotationType().isAnnotationPresent(Qualifier.class))
+                .findFirst().orElse(null);
+        return ComponentRef.of(field.getGenericType(),qualifier);
+
     }
 
     private static ComponentRef<?> toComponentRef(Parameter p) {
