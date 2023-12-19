72e0fe624f117ec58fdfa82ff92c7334981ca371
JiangTaoHou <925147954@qq.com>
2023-12-19 23:58:35 +0800

    1.获取依赖的方法进行改进，injectConstructor是单个的，变成stream，然后后面的就都取stream，因为后面俩是List，然后都取进行flatMap,然后把所有的依赖都再组合成stream,在变成toList();

--- diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
+++ diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
@@ -56,18 +56,10 @@ class InjectionProvider<T> implements ComponentProvider<T> {
 
     @Override
     public List<ComponentRef<?>> getDependencies() {
-        return concat(concat(stream(injectConstructor.require()),
-                        //为什么Field要转换为ComponentRef?因为我们要根据这些字段啊，构造器啊，方法啊这些地方获取参数
-                        //但是我们的字段上又是有东西的，比如@Qualifier，或者泛型，所以在Context调用get的时候，是获取components
-                        //那个Map上面的key,那个key，就是我们现在创建的ComponentRef
-//                        injectFields.stream().map(InjectionProvider::toComponentRef)),
-                        injectFields.stream().flatMap(f -> stream(f.require()))),
-
-                //因为Constructor直接就是可以获取数组，所以不用flatMap,然后InjectMethod是List，所以要使用Stream
-                //那为什么Map不行呢？因为后面的m.getParameterTypes()返回的还是数组，你要把它变成一维的，所以要使用flatMap
-//                injectMethods.stream().flatMap(p -> stream(p.getParameters()).map(InjectionProvider::toComponentRef)))
-                injectMethods.stream().flatMap(m -> stream(m.require())))
-                .toList();
+        //injectConstructor是单个的，变成stream，然后后面的就都取stream，因为后面俩是List，然后都取进行flatMap
+        //然后把所有的依赖都再组合成stream,在变成toList();
+        return concat(concat(Stream.of(injectConstructor), injectMethods.stream()), injectFields.stream())
+                .flatMap(i -> stream(i.require())).toList();
     }
 
     record Injectable<Element extends AccessibleObject>(Element element, ComponentRef<?>[] require) {
@@ -125,6 +117,7 @@ class InjectionProvider<T> implements ComponentProvider<T> {
         Collections.reverse(injectMethods);
         return injectMethods.stream().map(Injectable::of).toList();
     }
+
     private static <T> List<T> traverse(Class<?> component, BiFunction<List<T>, Class<?>, List<T>> finder) {
         List<T> members = new ArrayList<>();
         Class<?> current = component;
