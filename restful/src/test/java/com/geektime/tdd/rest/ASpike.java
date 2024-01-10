3adfd11979b4fe1a90cca22ff1c941b5972f746a
JiangTaoHou <925147954@qq.com>
2024-01-10 23:33:41 +0800

    1.使用我们自定义的MessageBodyWriter,目前还是返回String，输出流也还是使用的response的outputStream

--- restful/src/test/java/com/geektime/tdd/rest/ASpike.java
+++ restful/src/test/java/com/geektime/tdd/rest/ASpike.java
@@ -1,12 +1,15 @@
 package com.geektime.tdd.rest;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.*;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -17,15 +20,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Arrays;
-import java.util.Objects;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -42,7 +47,11 @@ public class ASpike {
         server.addConnector(connector);
 
         ServletContextHandler handler = new ServletContextHandler(server, "/");
-        handler.addServlet(new ServletHolder(new ResourceServlet(new TestApplication())), "/");
+        //刚开始这个TestApplication中是存储Controller，使用java自带的流去输出，现在要增加使用哪种方式去写出去
+        //TestProviders里面有各种可以提供的，context的，messageBodyWriter的，还有Exception的，但是这些对象
+        //放在哪呢?目前是放在TestApplication里面，所以new TestProviders(application)需要把application传送进去
+        TestApplication application = new TestApplication();
+        handler.addServlet(new ServletHolder(new ResourceServlet(application, new TestProviders(application))), "/");
         server.setHandler(handler);
         server.start();
     }
@@ -64,17 +73,26 @@ public class ASpike {
     static class ResourceServlet extends HttpServlet {
         private Application application;
 
-        public ResourceServlet(Application application) {
+        private Providers providers;
+
+        public ResourceServlet(Application application, Providers providers) {
             this.application = application;
+            this.providers = providers;
         }
 
         @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
             Stream<Class<?>> classStream = application.getClasses().stream().filter(c -> c.isAnnotationPresent(Path.class));
             Object result = dispatch(req, classStream);
 //            String result = new TestResource().get();
-            resp.getWriter().write(result.toString());
-            resp.getWriter().flush();
+            //这个providers里面已经通过application取出来所有的符合MessageBodyWriter，目前就只有一个
+            // StringMessageBodyWriter，然后getMessageBodyReader是获取，现在其实也就是获取到只有的那一个
+            //第一个参数就是要写的对象
+            MessageBodyWriter<Object> writer = (MessageBodyWriter<Object>) providers.getMessageBodyWriter(result.getClass(), null, null, null);
+            writer.writeTo(result, null, null, null, null, null, resp.getOutputStream());
+
+//            resp.getWriter().write(result.toString());
+//            resp.getWriter().flush();
         }
 
         Object dispatch(HttpServletRequest req, Stream<Class<?>> classStream) {
@@ -87,7 +105,7 @@ public class ASpike {
                 Method method = Arrays.stream(rootClass.getMethods()).filter(m -> m.isAnnotationPresent(GET.class)).findFirst().get();
                 //执行方法，那有返回值，就是返回的String
                 return method.invoke(rootResource);
-            }  catch (Exception e) {
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
@@ -96,7 +114,66 @@ public class ASpike {
     static class TestApplication extends Application {
         @Override
         public Set<Class<?>> getClasses() {
-            return Set.of(TestResource.class);
+            return Set.of(TestResource.class, StringMessageBodyWriter.class);
+        }
+    }
+
+    static class TestProviders implements Providers {
+        private List<MessageBodyWriter> writers;
+        private Application application;
+
+        public TestProviders(Application application) {
+            this.application = application;
+            //表示判断类是否是 MessageBodyWriter 接口的子类或实现类,如果是，则保留在流中。
+            //注意，这里是要强转的，比如规定泛型是MessageBodyWriter，否则泛型就是?没有泛型，下面就调用不了w.isWriteable方法
+            writers = (List<MessageBodyWriter>) this.application.getClasses().stream().filter(MessageBodyWriter.class::isAssignableFrom)
+                    .map(c -> {
+                        try {
+                            return c.getConstructor().newInstance();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).toList();
+        }
+
+        @Override
+        public <T> MessageBodyReader<T> getMessageBodyReader(Class<T> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+            return null;
+        }
+
+        @Override
+        public <T> MessageBodyWriter<T> getMessageBodyWriter(Class<T> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+            //就找到一个就行了，我们目前也是先找到一个
+            return writers.stream().filter(w -> w.isWriteable(type, genericType, annotations, mediaType)).findFirst().get();
+        }
+
+        @Override
+        public <T extends Throwable> ExceptionMapper<T> getExceptionMapper(Class<T> type) {
+            return null;
+        }
+
+        @Override
+        public <T> ContextResolver<T> getContextResolver(Class<T> contextType, MediaType mediaType) {
+            return null;
+        }
+    }
+
+    static class StringMessageBodyWriter implements MessageBodyWriter<String> {
+        public StringMessageBodyWriter() {
+        }
+
+        @Override
+        public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+            return type == String.class;
+        }
+
+        @Override
+        public void writeTo(String s, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+            //entityStream: 用于写入消息体的输出流。
+            PrintWriter writer = new PrintWriter(entityStream);
+            //s: 要写入响应消息体的对象实例。
+            writer.write(s);
+            writer.flush();
         }
     }
 
