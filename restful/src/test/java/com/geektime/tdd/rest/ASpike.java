5f7a63139cb0ba7202635b68862b74572800f250
JiangTaoHou <925147954@qq.com>
2024-01-10 22:44:09 +0800

    1.TestApplication的getClass就是获取所有的Controller的类 2.ResourceServlet替換成原來的直接new出來的

--- restful/src/test/java/com/geektime/tdd/rest/ASpike.java
+++ restful/src/test/java/com/geektime/tdd/rest/ASpike.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Application;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -21,6 +22,7 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -34,15 +36,7 @@ public class ASpike {
         server.addConnector(connector);
 
         ServletContextHandler handler = new ServletContextHandler(server,"/");
-        handler.addServlet(new ServletHolder(new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-                //目前测试通过，和@Path没关系呢，就是简单的返回String字符串
-                String result = new TestResource().get();
-                resp.getWriter().write(result);
-                resp.getWriter().flush();
-            }
-        }),"/");
+        handler.addServlet(new ServletHolder(new ResourceServlet(new TestApplication())),"/");
         server.setHandler(handler);
         server.start();
     }
@@ -58,6 +52,28 @@ public class ASpike {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
         assertEquals("test",response.body());
+    }
+
+    static class ResourceServlet extends HttpServlet{
+        private Application application;
+
+        public ResourceServlet(Application application) {
+            this.application = application;
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String result = new TestResource().get();
+            resp.getWriter().write(result);
+            resp.getWriter().flush();
+        }
+    }
+
+    static class TestApplication extends Application{
+        @Override
+        public Set<Class<?>> getClasses() {
+            return Set.of(TestResource.class);
+        }
     }
 
     @Path("/test")
