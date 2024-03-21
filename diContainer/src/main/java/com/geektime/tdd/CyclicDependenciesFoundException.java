9ad5a3f8ad2b84e9fc93aecf3bb89a3431835856
JiangTaoHou <925147954@qq.com>
2024-03-21 23:36:57 +0800

    1.为什么这里还是返回一个Class，不返回Component，因为返回Component的话，就不存在循环依赖了，都有了Qualifier，就不是循环依赖了，这个是给之前的没有Qualifier使用的，

--- diContainer/src/main/java/com/geektime/tdd/CyclicDependenciesFoundException.java
+++ diContainer/src/main/java/com/geektime/tdd/CyclicDependenciesFoundException.java
@@ -14,5 +14,8 @@ public class CyclicDependenciesFoundException extends RuntimeException{
 
     public Class<?>[] getComponents() {
         return components.stream().map(c->c.type()).toArray(Class[]::new);
+        //为什么这里还是返回一个Class，不返回Component，因为返回Component的话，就不存在循环依赖了，都有了Qualifier，就
+        //不是循环依赖了，这个是给之前的没有Qualifier使用的，
+//        return components;
     }
 }
