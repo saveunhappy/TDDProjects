6216d5ffc5cc9ab8be978537f19556f8607dfd74
JiangTaoHou <925147954@qq.com>
2024-04-14 01:37:21 +0800

    1.不加这两个slf4j的依赖，报错出现不了java.lang.IllegalArgumentException                       	at org.eclipse.jetty.server.Response.setStatus(Response.java:750)这个错误

--- restful/src/test/java/com/geektime/tdd/rest/ResourceServletTest.java
+++ restful/src/test/java/com/geektime/tdd/rest/ResourceServletTest.java
@@ -61,8 +61,7 @@ public class ResourceServletTest extends ServletTest {
          * 这个时候在build的时候报错：Provider for jakarta.ws.rs.ext.RuntimeDelegate cannot be found
          * 因为构建的时候是用RuntimeDelegate这个类的，那么我们也给stub掉
          * */
-        NewCookie sessionId = new NewCookie.Builder("SESSION_ID").value("session").build();
-        NewCookie userId = new NewCookie.Builder("USER_ID").value("user").build();
+
         //先是mock掉，其实和创建一个子类没啥区别
         RuntimeDelegate delegate = mock(RuntimeDelegate.class);
         //然后设置全局的
@@ -81,17 +80,21 @@ public class ResourceServletTest extends ServletTest {
             }
         });
 
+        NewCookie sessionId = new NewCookie.Builder("SESSION_ID").value("session").build();
+        NewCookie userId = new NewCookie.Builder("USER_ID").value("user").build();
         OutboundResponse response = mock(OutboundResponse.class);
         MultivaluedMap<String,Object> headers = new MultivaluedHashMap<>();
         // key是Set-Cookie，value是一个list,就是SESSION_ID和USER_ID
 
         headers.addAll("Set-Cookie", sessionId, userId);
+//        when(response.getStatus()).thenReturn(Response.Status.NOT_MODIFIED.getStatusCode());
         when(response.getHeaders()).thenReturn(headers);
         when(router.dispatch(any(), eq(resourceContext))).thenReturn(response);
         // 这个get就是HttpRequest 发送的，然后得到HttpResponse
         HttpResponse<String> httpResponse = get("/test");
 
-        assertArrayEquals(new String[]{"SESSION_ID=session","USER_ID=user"},httpResponse.headers().allValues("Set-Cookie").toArray(String[]::new));
+        assertArrayEquals(new String[]{"SESSION_ID=session","USER_ID=user"},
+                httpResponse.headers().allValues("Set-Cookie").toArray(String[]::new));
     }
 
     //TODO: writer body using MessageBodyWriter
