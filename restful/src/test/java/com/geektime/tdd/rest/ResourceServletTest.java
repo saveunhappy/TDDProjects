b0b7659154d297179776764030467c744c9805f4
JiangTaoHou <925147954@qq.com>
2024-04-14 00:59:31 +0800

    1.这个时候在build的时候报错：Provider for jakarta.ws.rs.ext.RuntimeDelegate cannot be found，因为构建的时候是用RuntimeDelegate这个类的，那么我们也给stub掉

--- restful/src/test/java/com/geektime/tdd/rest/ResourceServletTest.java
+++ restful/src/test/java/com/geektime/tdd/rest/ResourceServletTest.java
@@ -2,12 +2,16 @@ package com.geektime.tdd.rest;
 
 import jakarta.servlet.Servlet;
 import jakarta.ws.rs.container.ResourceContext;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.net.http.HttpResponse;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -50,6 +54,28 @@ public class ResourceServletTest extends ServletTest {
     }
 
     //TODO: use headers as http header
+    @Test
+    public void should_use_http_headers_from_response() throws Exception {
+        /*
+         * 这个时候在build的时候报错：Provider for jakarta.ws.rs.ext.RuntimeDelegate cannot be found
+         * 因为构建的时候是用RuntimeDelegate这个类的，那么我们也给stub掉
+         * */
+        NewCookie sessionId = new NewCookie.Builder("SESSION_ID").value("session").build();
+        NewCookie userId = new NewCookie.Builder("USER_ID").value("user").build();
+
+        OutboundResponse response = mock(OutboundResponse.class);
+        MultivaluedMap<String,Object> headers = new MultivaluedHashMap<>();
+        // key是Set-Cookie，value是一个list,就是SESSION_ID和USER_ID
+
+        headers.addAll("Set-Cookie", sessionId, userId);
+        when(response.getHeaders()).thenReturn(headers);
+        when(router.dispatch(any(), eq(resourceContext))).thenReturn(response);
+        // 这个get就是HttpRequest 发送的，然后得到HttpResponse
+        HttpResponse<String> httpResponse = get("/test");
+
+        assertArrayEquals(new String[]{sessionId.toString(),userId.toString()},httpResponse.headers().allValues("Set-Cookie").toArray(String[]::new));
+    }
+
     //TODO: writer body using MessageBodyWriter
     //TODO: 500 if MessageBodyWriter not found
     //TODO: throw WebApplicationException with response,use response
