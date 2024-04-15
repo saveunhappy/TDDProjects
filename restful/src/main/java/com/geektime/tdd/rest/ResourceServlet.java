009726860f3d312830235f5e59dba83a10fef7d5
JiangTaoHou <925147954@qq.com>
2024-04-15 23:11:58 +0800

    1.替换成方法，测试通过

--- restful/src/main/java/com/geektime/tdd/rest/ResourceServlet.java
+++ restful/src/main/java/com/geektime/tdd/rest/ResourceServlet.java
@@ -27,30 +27,27 @@ public class ResourceServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ResourceRouter router = runtime.getResourceRouter();
-        OutboundResponse response;
         try {
             //看下这个结构，我们是先声明了一个OutboundResponse，然后不管是正常还是异常，都是返回了一个response，
             // 然后最后再去调用那个方法，那么其实可以替换成每个都去调用，就不用最后处理了，在重构那本书中这个是个坏味道，
             // 但是在这里是个好方法，因为我们这里有递归，重构的案例是个普通的方法
 //            respond(resp, response);
-//            respond(resp, router.dispatch(req, runtime.createResourceContext(req, resp)));
-            response = router.dispatch(req, runtime.createResourceContext(req, resp));
+            respond(resp, router.dispatch(req, runtime.createResourceContext(req, resp)));
+//            response = router.dispatch(req, runtime.createResourceContext(req, resp));
         } catch (WebApplicationException exception) {
             //注意，异常的构造器就是接收一个response，而且是在所有stub之后的，所以状态码什么的已经设置过了
-            response = (OutboundResponse) exception.getResponse();
+            respond(resp, (OutboundResponse) exception.getResponse());
         } catch (Throwable throwable) {
-            try{
+            try {
                 ExceptionMapper exceptionMapper = providers.getExceptionMapper(throwable.getClass());
-                response = (OutboundResponse) exceptionMapper.toResponse(throwable);
-            }catch (WebApplicationException exception){
-                response = (OutboundResponse) exception.getResponse();
-            }catch (Throwable throwable1){
+                respond(resp, (OutboundResponse) exceptionMapper.toResponse(throwable));
+            } catch (WebApplicationException exception) {
+                respond(resp, (OutboundResponse) exception.getResponse());
+            } catch (Throwable throwable1) {
                 ExceptionMapper exceptionMapper = providers.getExceptionMapper(throwable1.getClass());
-                response = (OutboundResponse) exceptionMapper.toResponse(throwable1);
+                respond(resp, (OutboundResponse) exceptionMapper.toResponse(throwable1));
             }
-
         }
-        respond(resp, response);
 
     }
 
