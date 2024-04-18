ae35396a425ebb5747db9b4f56891a3099581517
JiangTaoHou <925147954@qq.com>
2024-04-18 23:39:04 +0800

    1.抽取方法

--- restful/src/test/java/com/geektime/tdd/rest/ResourceServletTest.java
+++ restful/src/test/java/com/geektime/tdd/rest/ResourceServletTest.java
@@ -181,10 +181,7 @@ public class ResourceServletTest extends ServletTest {
     @Test
     public void should_use_response_from_web_application_exception_thrown_by_providers_when_find_message_body_writer() throws Exception {
         RuntimeException exception = new WebApplicationException(response().status(Response.Status.FORBIDDEN).build());
-        response.entity(new GenericEntity<>(2.5,Double.class),new Annotation[0]).returnFrom(router);
-
-        when(providers.getMessageBodyWriter(eq(Double.class),eq(Double.class),
-                eq(new Annotation[0]),eq(MediaType.TEXT_PLAIN_TYPE))).thenThrow(exception);
+        providersGetMessageBodyWriterThrows(exception);
 
         when(providers.getExceptionMapper(eq(IllegalArgumentException.class))).thenReturn(e ->
                 response().status(Response.Status.FORBIDDEN).build()
@@ -197,9 +194,7 @@ public class ResourceServletTest extends ServletTest {
     @Test
     public void should_map_exception_thrown_by_providers_when_find_message_body_writer() throws Exception {
         RuntimeException exception = new IllegalArgumentException();
-        response.entity(new GenericEntity<>(2.5,Double.class),new Annotation[0]).returnFrom(router);
-        when(providers.getMessageBodyWriter(eq(Double.class),eq(Double.class),
-                eq(new Annotation[0]),eq(MediaType.TEXT_PLAIN_TYPE))).thenThrow(exception);
+        providersGetMessageBodyWriterThrows(exception);
 
         when(providers.getExceptionMapper(eq(IllegalArgumentException.class))).thenReturn(e ->
                 response().status(Response.Status.FORBIDDEN).build()
@@ -209,6 +204,13 @@ public class ResourceServletTest extends ServletTest {
         assertEquals(Response.Status.FORBIDDEN.getStatusCode(), httpResponse.statusCode());
 
     }
+
+    private void providersGetMessageBodyWriterThrows(RuntimeException exception) {
+        response.entity(new GenericEntity<>(2.5,Double.class),new Annotation[0]).returnFrom(router);
+        when(providers.getMessageBodyWriter(eq(Double.class),eq(Double.class),
+                eq(new Annotation[0]),eq(MediaType.TEXT_PLAIN_TYPE))).thenThrow(exception);
+    }
+
     @Test
     public void should_use_response_from_web_application_exception_thrown_by_message_body_writer() throws Exception {
         RuntimeException exception = new WebApplicationException(response()
