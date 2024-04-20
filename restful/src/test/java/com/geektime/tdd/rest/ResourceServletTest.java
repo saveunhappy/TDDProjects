8e6be090d5127283cf46f98029d852486d030aad
JiangTaoHou <925147954@qq.com>
2024-04-21 01:52:29 +0800

    1.抽取方法

--- restful/src/test/java/com/geektime/tdd/rest/ResourceServletTest.java
+++ restful/src/test/java/com/geektime/tdd/rest/ResourceServletTest.java
@@ -183,18 +183,7 @@ public class ResourceServletTest extends ServletTest {
     public void should_use_response_from_web_application_exception_thrown_by_header_delegate_when_create_header_delegate(){
         WebApplicationException exception = new WebApplicationException(response().
                 status(Response.Status.FORBIDDEN).build());
-        response().headers(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_TYPE).returnFrom(router);
-        when(delegate.createHeaderDelegate(eq(MediaType.class))).thenReturn(new RuntimeDelegate.HeaderDelegate<MediaType>() {
-            @Override
-            public MediaType fromString(String value) {
-                return null;
-            }
-
-            @Override
-            public String toString(MediaType value) {
-                throw exception;
-            }
-        });
+        headerDelegate_toString(exception);
 
         HttpResponse<String> httpResponse = get("/test");
         assertEquals(Response.Status.FORBIDDEN.getStatusCode(), httpResponse.statusCode());
@@ -205,18 +194,7 @@ public class ResourceServletTest extends ServletTest {
     public void should_map_exception_thrown_by_header_delegate_when_create_header_delegate() {
         RuntimeException exception = new IllegalArgumentException();
 
-        response().headers(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_TYPE).returnFrom(router);
-        when(delegate.createHeaderDelegate(eq(MediaType.class))).thenReturn(new RuntimeDelegate.HeaderDelegate<MediaType>() {
-            @Override
-            public MediaType fromString(String value) {
-                return null;
-            }
-
-            @Override
-            public String toString(MediaType value) {
-                throw exception;
-            }
-        });
+        headerDelegate_toString(exception);
         //因为这个IllegalArgumentsException不能被WebApplicationException捕获，
         // 只能被Throwable捕获，这个其实是相当于你自定义的了，
         // 那你自定义的就得你自己创建处理的方式，就是你自己注册到ExceptionMapper了，
@@ -228,6 +206,21 @@ public class ResourceServletTest extends ServletTest {
         HttpResponse<String> httpResponse = get("/test");
         assertEquals(Response.Status.FORBIDDEN.getStatusCode(), httpResponse.statusCode());
 
+    }
+
+    private void headerDelegate_toString(RuntimeException exception) {
+        response().headers(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_TYPE).returnFrom(router);
+        when(delegate.createHeaderDelegate(eq(MediaType.class))).thenReturn(new RuntimeDelegate.HeaderDelegate<MediaType>() {
+            @Override
+            public MediaType fromString(String value) {
+                return null;
+            }
+
+            @Override
+            public String toString(MediaType value) {
+                throw exception;
+            }
+        });
     }
 
     @TestFactory
